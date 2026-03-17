4072acf6ce8a92341c1702c2cf3e9cf55bd53d0e
NingTr <149238761+NingTr@users.noreply.github.com>
2026-03-17 19:01:06 +0700

    Delete backend/test.py

--- backend/test.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBTSq4lNMrHzOm5fiCIARkf4MzJltQ_OAw")
-
-for m in genai.list_models():
-    if "generateContent" in m.supported_generation_methods:
-        print(m.name)
